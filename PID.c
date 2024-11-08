e2dddf032c014ca586bc4b3ecd77dc992021c459
Ruhan456 <63120454+Ruhan456@users.noreply.github.com>
2024-11-08 12:27:27 -0500

    Update PID.c

--- PID.c
+++ PID.c
@@ -54,11 +54,11 @@ void readFile(int &rescueX, int &rescueY, int &saveX, int &saveY) {
 
 void configureAllSensors ()
 {
-	SensorType[S4] = sensorEV3_Gyro;
+	SensorType[S1] = sensorEV3_Gyro;
 	wait1Msec(50);
-	SensorMode[S4] = modeEV3Gyro_Calibration;
+	SensorMode[S1] = modeEV3Gyro_Calibration;
 	wait1Msec(50);
-	SensorMode[S4] = modeEV3Gyro_RateAndAngle;
+	SensorMode[S1] = modeEV3Gyro_RateAndAngle;
 	wait1Msec(50);
 	SensorMode[S3] = sensorEV3_Color;
 	wait1Msec(50);
