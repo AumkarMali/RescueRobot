3833745a7b12afb3fa011b2bb0a085d5b9316a50
Arthigan-J <arthiganjm@gmail.com>
2024-11-15 17:40:54 -0500

    Update PID.c

--- PID.c
+++ PID.c
@@ -1,61 +1,76 @@
 #pragma config(Motor, motorB, leftMotor, tmotorEV3_Large, PIDControl, encoder)
 #pragma config(Motor, motorC, rightMotor, tmotorEV3_Large, PIDControl, encoder)
+#include "PC_FileIO.c"
+#include "EV3Servo-lib-UW.c"
 
 //ROBOT C
 const int FULL_POWER = 100;
 const float WHEEL_RADIUS = 8.16 / 2;
 const float CONVERSION = PI * WHEEL_RADIUS / 180;
+const int MAX_POINTS = 8;
 
 void elevate(bool upOrDown) {
 	nMotorEncoder[motorC] = 0;
 	if(upOrDown){
 		motor[motorC] = -25;
-	}else{
+		}else{
 		motor[motorC] = 25;
 	}
 
-	while(abs(nMotorEncoder[motorC]) < 110) {}
+	while(abs(nMotorEncoder[motorC]) < 85) {}
 
 	motor[motorC] = 0;
 }
 
-float calculateAngle(float x1, float y1, float x2, float y2, float previous_angle) {
-    // Vector from (x1, y1) to (x2, y2)
-    float vector_x = x2 - x1;
-    float vector_y = y2 - y1;
+float getDegrees(float x1, float y1, float x2, float y2, float previous_angle) {
+	// Vector from (x1, y1) to (x2, y2)
+	float vector_x = x2 - x1;
+	float vector_y = y2 - y1;
 
-    // Calculate the angle of this vector relative to the positive x-axis
-    float angle_radians = atan2(vector_y, vector_x);
-    float angle_degrees = angle_radians * (180.0 / PI);
+	// Calculate the angle of this vector relative to the positive x-axis
+	float angle_radians = atan2(vector_y, vector_x);
+	float angle_degrees = angle_radians * (180.0 / PI);
 
-    // Calculate the smallest turn angle relative to the previous angle
-    float turn_angle = angle_degrees - previous_angle;
+	// Calculate the smallest turn angle relative to the previous angle
+	float turn_angle = angle_degrees - previous_angle;
 
-    // Normalize to be within -180 to 180 degrees for shortest rotation
-    if (turn_angle > 180) {
-        turn_angle -= 360;
-    } else if (turn_angle < -180) {
-        turn_angle += 360;
-    }
-
-    return turn_angle;
+	// Normalize to be within -180 to 180 degrees for shortest rotation
+	if (turn_angle > 180) {
+		turn_angle -= 360;
+		} else if (turn_angle < -180) {
+		turn_angle += 360;
+	}
+	/*
+	if (turn_angle<0)
+	{
+		return turn_angle+3;
+	} else {
+		return turn_angle-3;
+	}
+	*/
+	return turn_angle;
 }
-
 
 
 void rotate(float deg) {
 	resetGyro(S1);
 	//CCW
+	int direction = 0;
 	if (deg >= 0) {
-		motor[motorA] = FULL_POWER;
-		motor[motorD] = -1 * FULL_POWER;
+		direction = 20;
 	}
 	//CW
 	else {
-		motor[motorA] = -1 * FULL_POWER;
-		motor[motorD] = FULL_POWER;
+		direction = -20;
 	}
-	while (abs(getGyroDegrees(S1)) < deg) {}
+	motor[motorA] = direction;
+	motor[motorD] = direction*-1;
+	while (abs(getGyroDegrees(S1)) < abs(deg))
+		{
+			float error = 1.1 - abs(getGyroDegrees(S1))/deg;
+			motor[motorA] = direction*error;
+			motor[motorD] = (-1*direction)*error;
+		}
 
 	motor[motorA] = motor[motorD] = 0;
 }
@@ -68,108 +83,164 @@ void configureAllSensors ()
 	wait1Msec(50);
 	SensorMode[S1] = modeEV3Gyro_RateAndAngle;
 	wait1Msec(50);
-	SensorMode[S3] = sensorEV3_Color;
+	SensorType[S3] = sensorI2CCustom9V;
 	wait1Msec(50);
-	SensorMode[S3] = modeEV3Color_Color;
+	SensorMode[S4] = sensorEV3_Color;
 	wait1Msec(50);
+	SensorMode[S4] = modeEV3Color_Color;
+	wait1Msec(50);
+
 }
 
-moveBot(int rescueX, int rescueY, int saveX, int saveY, int counter, float angle){
-	dist = sqrt(pow((saveX-rescueX),2)+pow((saveY-rescueY)));
-	
+void adjustBot(float distance)
+{
 	nMotorEncoder[motorA] = 0;
-	motor[motorA] = motor[motorD] = FULL_POWER;
-	
-	//For loop for slowing down bot
-	// while(sensorValue[S2] == (int)colorRed)
-	// 	{}
-	// for ()
-	// {
-	// 	}
-	
-	while(abs(nMotorEncoder[motorA]) * CONVERSION < dist)
-		{}
+	motor[motorA] = motor[motorD] = 20;
+
+	wait1Msec(100);
+
+	for(int i = 100 ; i>15 ; i --){
+		motor[motorA] = motor[motorD] = i;
+	}
+
+	while((abs(nMotorEncoder[motorA]) * CONVERSION) < abs(distance))
+	{}
+
 	motor[motorA] = motor[motorD] = 0;
 }
 
-operateClaw(){
-	motor[motorB] = 25;//assuming positive is closing the clamp
-	while(getSensorValue[s2] != (int)colorBlue)
-		{}
-	motor[motorB] = -25;
-	wait1Msec(1000);
-	motor[motorB] = 0;
-	openClose = "close";
+void moveBot(int rescueX, int rescueY, int saveX, int saveY, bool pickDrop){
+	float dist = sqrt(pow((saveX-rescueX),2)+pow((saveY-rescueY),2))*15;
+
+	nMotorEncoder[motorA] = 0;
+	motor[motorA] = motor[motorD] = FULL_POWER;
+
+	//for(int i = FULL_POWER ; i>15 ; i--){
+	//	motor[motorA] = motor[motorD] = i;
+
+	//}
+
+	if (!pickDrop){
+		float i = FULL_POWER;
+		while(SensorValue[S4]!=(int)colorBlue){ // new pid add the other side of if
+			if((i>15) && (abs(nMotorEncoder[motorA])*CONVERSION)> 0.5){
+				motor[motorA] = motor[motorD] = i;
+				if (SensorValue[S4]!=(int)colorBlue){
+					wait1Msec(dist/abs(nMotorEncoder[motorA])*CONVERSION);
+				}
+				i--;
+			}
+		}
+
+
+		while(SensorValue[S4]!=(int)colorBlue)
+			{}
+	}else{
+		while(SensorValue[S4]!=(int)colorRed)
+			{}
+	}
+
+	motor[motorA] = motor[motorD] = 15;
+	while(abs(nMotorEncoder[motorA]) * CONVERSION < (dist-21.5))
+	{}
+
+	motor[motorA] = motor[motorD] = 0;
 }
+
+
+void operateClaw(bool openOrClose){
+	if(openOrClose){
+		setServoPosition(S3, 3, 65);
+		}else{
+		setServoPosition(S3, 3, 10);
+	}
+}
+
 
 void fileRead(TFileHandle read)
 {
-	float pickX = 0, pickY = 0, dropX = 0, dropY = 0, prevX = 0, prevY = 0;
-	readFloatPC(read, pickX);
-	readFloatPC(read, pickY);
-	readFloatPC(read, dropX);
-	readFloatPC(read, dropY);
+	float prevAngle = 0;
 	float angle = 0;
+	float driveSum = 0;
+	float adjustDist = -21.5;
 
-	bool up = 0;
-	bool down = 1;
+	float displayAngle = 0;
 
-	operateClaw("open");
-	evevate("up");
-	for (int i = 0; i < 3; i++){
-			angle = getDegrees(prevX, prevY, pickX, pickY);
-			rotate(angle);	
-			moveBot(prevX, prevY, pickX, pickY);
-			elevate(down);
-			operateClaw("close");
-			elevate(up);
-			angle = getDegrees(pickX, pickY, dropX, dropY);
-			rotate(angle);
-			moveBot(pickX, pickY, dropX, dropY, i);
-			prevX = dropX;
-			prevY = dropY;
-			elevate(down);
-			operateClaw("open");
-			elevate(up);
+	float xcoords[MAX_POINTS];
+	float ycoords[MAX_POINTS];
+	xcoords[0] = 0;
+	ycoords[0] = 0;
+	xcoords[7] = 0;
+	ycoords[7] = 0;
+
+	for (int i = 1; i<8; i++)
+	{
+		readFloatPC(read, xcoords[i]);
+		readFloatPC(read, ycoords[i]);
+	}
+
+	bool up = false;
+	bool down = true;
+	bool Claw = true;
+
+	operateClaw(Claw);
+
+	clearTimer(T1);
+
+	for (int i = 0; i<7; i++)
+	{
+
+		if (i==0 || i==2 || i==4)
+		{
+			Claw = false;
+		} else if (i==1 || i==3 || i==5)
+		{
+			Claw = true;
 		}
-	//For Angle stuff
-	 // Define the x and y coordinates of each waypoint
- // Define the x and y coordinates of each waypoint
-    float x_coords[] = {0, -5, 0, -6, 3, 3, 9 , 0};
-    float y_coords[] = {0, 0, -1, -3, 3, 5, 4 , 0};
-    int num_waypoints = sizeof(x_coords) / sizeof(x_coords[0]);
 
-    // Calculate angles between consecutive vectors and display them
-    for (int i = 0; i < num_waypoints - 2; i++) {
-        float angle = calculateAngle(
-            x_coords[i], y_coords[i],      // (x1, y1)
-            x_coords[i + 1], y_coords[i + 1],  // (x2, y2)
-            x_coords[i + 2], y_coords[i + 2]   // (x3, y3)
-        );
+		if (i==6)
+		{
+			adjustDist = 0;
+		}
 
-        // Display the angle (smallest possible turning angle)
-        displayTextLine(i, "Angle %d -> %d -> %d: %.2f deg", i, i + 1, i + 2, angle);
-        wait1Msec(1000);    // Pause briefly to view each angle
-    }
+		angle = getDegrees(xcoords[i], ycoords[i], xcoords[i+1], ycoords[i+1], prevAngle);
+		displayTextLine(5, "%f", angle);
+		prevAngle += angle;
 
-    wait1Msec(10000); // Wait to view all results on screen
 
-	
+		wait1Msec(1000);
+		rotate(angle);
+
+
+		wait1Msec(1000);
+		moveBot(xcoords[i], ycoords[i], xcoords[i+1], ycoords[i+1], Claw);
+
+
+		driveSum += abs(nMotorEncoder[motorA])*CONVERSION;
+		wait1Msec(1000);
+		elevate(down);
+		wait1Msec(1000);
+		operateClaw(Claw);
+		elevate(up);
+		adjustBot(adjustDist);
+		wait1Msec(1000);
+	}
+
+	float driveTime = time1[T1]/1000.0;
+	resetGripper(S3, 3);
+	displayTextLine(4, "Time of Rescue: %f", driveTime);
+	displayTextLine(5, "Total Distance Travelled: %fcm/s", driveSum);
+	wait1Msec(10000);
 }
 
 
 task main()
 {
 
-	configureSensors();
+	configureAllSensors();
 
-	//Wait for enter button to start
-	while(!getButtonPress(buttonEnter)) 
-		{}
-	while(getButtonPress(buttonEnter))
-		{}
 	wait1Msec(1000);
-	float x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+	float x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0;
 	string space = " ";
 
 	TFileHandle read;
@@ -179,7 +250,11 @@ task main()
 	{
 		displayString(5, "Error!");
 		wait1Msec(5000);
-	} else {
+		} else {
+		while(!getButtonPress(buttonAny)) //Wait for enter button to start
+		{}
+		while(getButtonPress(buttonAny))
+		{}
 		fileRead(read);
 	}
 	closeFilePC(read);
